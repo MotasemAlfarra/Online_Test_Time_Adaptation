c408149fcb4c0a5def31d3d559807d446f2c8d66
MotasemAlfarra <alfarrm@gpu212-06.ibex.kaust.edu.sa>
2023-04-06 09:46:39 +0300

    removing redundant files

--- utils/memo_transforms.py
+++
@@ -1,5 +0,0 @@
-import torchvision.transforms as transforms
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-te_transforms_inc = transforms.Compose([transforms.CenterCrop(224),
-                                        transforms.ToTensor(),
-                                        normalize])
